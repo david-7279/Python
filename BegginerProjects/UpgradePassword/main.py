04897205df4a13b1d59511f1b62453c8bf158659
David Vieira <davidsoares7279@gmail.com>
2025-03-19 14:55:05 +0000

    feat: upgrade password structure

---
+++ BegginerProjects/UpgradePassword/main.py
@@ -0,0 +1,6 @@
+from time import sleep
+import os
+
+# Main Program
+if __name__== "__main__":
+  print("Hello World")
---
+++ BegginerProjects/UpgradePassword/models/__init__.py
@@ -0,0 +1,5 @@
+from .weak import Weak
+from .moderate import Moderate
+from .strong import Strong
+
+__all__ = ["Weak", "Moderate", "Strong"]
