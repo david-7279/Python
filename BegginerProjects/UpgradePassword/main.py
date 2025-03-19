543551158c31477ff27b4d02e8bf9406e62b1651
David Vieira <davidsoares7279@gmail.com>
2025-03-19 15:23:38 +0000

    feat: leave main_menu logic

--- BegginerProjects/UpgradePassword/main.py
+++ BegginerProjects/UpgradePassword/main.py
@@ -1,5 +1,6 @@
 from models.menu import main_menu
 from models.clear_screen import clear
+from time import sleep
 
 def main_menu_option():
     choices = [1, 2, 3, 0]
@@ -20,6 +21,7 @@ def main_menu_option():
 
 if __name__ == "__main__":
     while True:  
+      clear()
       main_menu()
       option = main_menu_option()
       clear()
@@ -32,6 +34,24 @@ if __name__ == "__main__":
           case 3:
               pass
           case 0:
-              pass
+              while True:
+                confirmation = input("Are you sure want to leave (Yes/No)? ").lower()
+                if confirmation in ['y', 'yes']:
+                    print("Exiting the program...")
+                    sleep(1)
+                    clear()
+                    exit()
+                elif confirmation in ['n', 'no']:
+                    print("I'm glad you are not leaving!")
+                    sleep(1)
+                    input("Press enter to continue...")
+                    clear()
+                    break
+                else:
+                    print(f"Invalid confirmation: {confirmation}! Please, choose an valid confirmation!")
+                    sleep(1)
+                    input("Press enter to continue...")
+                    clear()
+                    continue
           case _:
               pass
